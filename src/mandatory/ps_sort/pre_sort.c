70a4e77e9a10573b8fb73783f82245fe8e0d8ab4
Luis <lluque@c3r3s5.42malaga.com>
2024-08-13 17:29:43 +0200

    FIX2 infinite ra() loop in pre_sort() watchdog
    A watchdog function was added in pre_sort() to prevent any possible
    condition where the number of pushable elements left in stack A are
    less than that the number required to be pb()'ed in a block
    processing. This used to cause an infinite loop of ra()'s in the
    pre-sort stage for some random numbers combinations.

--- src/mandatory/ps_sort/pre_sort.c
+++ src/mandatory/ps_sort/pre_sort.c
@@ -6,7 +6,7 @@
 /*   By: lluque <lluque@student.42malaga.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 10:03:11 by lluque            #+#    #+#             */
-/*   Updated: 2024/08/11 21:28:37 by lluque           ###   ########.fr       */
+/*   Updated: 2024/08/13 17:09:04 by lluque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,12 @@ static void	set_current_avg_pos_when_sorted(t_ps_stacks *ps)
 		ret_val = temp;
 	ps->avg_pos_when_sorted = ret_val;
 	if (MAKE_DEBUG_LVL)
-		ft_printf("[pre_sort: set_current_avg_pos_when_sorted] %d\n", ret_val);
+	{
+		ft_printf("[pre_sort: set_current_avg_pos_when_sorted] avg = %d",
+				ret_val);
+		ft_printf(" Press enter to continue...\n");
+		read(0, (char *)&temp, 1);
+	}
 }
 
 static void	account_for_pushed(t_ps_stacks *ps, int *i)
@@ -47,13 +52,51 @@ static void	account_for_pushed(t_ps_stacks *ps, int *i)
 	*i = *i + 1;
 }
 
+static void loop_block_watchdog(t_ps_stacks *ps, int must_go)
+{
+	int			pushable_elements;
+	int			stack_a_size;
+	int			i;
+	t_dlclst	*current_node;
+	char		temp;
+
+	pushable_elements = 0;
+	stack_a_size = ft_dlclst_size(ps->a);
+	i = -1;
+	current_node = ps->a;
+	while (++i < stack_a_size)
+	{
+		if (get_pos_when_sorted(current_node->content)
+			< ps->avg_pos_when_sorted)
+			pushable_elements++;
+		current_node = current_node->next;
+	}
+	if (MAKE_DEBUG_LVL)
+		ft_printf("[pre_sort: loop_block_watchdog] ps->left_to_pre_sort  pushable_elements = %d  %d\n", ps->left_to_pre_sort, pushable_elements);
+	if (must_go > pushable_elements)
+	{
+		ps->avg_pos_when_sorted++;
+		if (MAKE_DEBUG_LVL)
+		{
+			ft_printf("[pre_sort: loop_block_watchdog] avg decreased to = %d\n",
+					ps->avg_pos_when_sorted);
+			ft_printf(" Press enter to continue...\n");
+			read(0, (char *)&temp, 1);
+		}
+	}
+}
+
+// Added a watchdog:
+// If ps->left_to_pre_sort is greater than the number of elements that matches
+// the < ps->avg_pos_when_sorted criteria, then lower the criteria.
 static void	loop_block(t_ps_stacks *ps, int block_size)
 {
 	int			i;
 
 	i = 0;
 	while (i < block_size)
-	{
+	{	
+		loop_block_watchdog(ps, block_size - i);
 		if (get_pos_when_sorted(top(A, ps)) < ps->avg_pos_when_sorted)
 		{
 			pb(ps);
